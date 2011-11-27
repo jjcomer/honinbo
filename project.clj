ce4b3abbc240402394109240e724b52a2e18584d
Josh Comer <joshjcomer@gmail.com>
2011-11-27 11:03:24 -0400

    Added the start fo documentation and placeholders for UI

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject honinbo "0.0.1-SNAPSHOT"
   :description "A Go client client in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [seesaw "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [seesaw "1.2.1"]]
+  :dev-dependencies [[lein-marginalia "0.7.0-SNAPSHOT"]])
--- src/honinbo/core.clj
+++ src/honinbo/core.clj
@@ -1,1 +1,14 @@
-(ns honinbo.core)
+;; #Honinbo
+;; A multi-platform Go client written in Clojure
+;;
+(ns honinbo.core
+  (:use [seesaw.core]))
+
+(defn -main
+  "This is the main entry point for launching honinbo **place holder**"
+  [ & args ]
+  (-> (frame :title "hoinibo"
+             :on-close :exit
+             :content "Hello World!")
+      pack!
+      show!))
