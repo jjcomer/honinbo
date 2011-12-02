ead2c9b0755c3880f5de1ab9fb1034a383977bcd
Josh Comer <joshjcomer@gmail.com>
2011-12-01 20:16:14 -0400

    Fixed comments to work with marginalia

--- src/honinbo/core.clj
+++ src/honinbo/core.clj
@@ -4,11 +4,32 @@
 (ns honinbo.core
   (:use [seesaw.core]))
 
+(defn build-board
+  "This function will add the game board to the UI
+
+  _size_: The size of the game board. Can be 9, 13, or 19
+
+  **TODO** This function still needs to be written"
+  [size]
+  :true)
+
+(defn build-controls
+  "This function will create the non board elements of the UI
+   **TODO** This function still needs to be written"
+  [ ]
+  :true )
+
+(defn build-ui
+  "Build the main ui frame."
+  [ ]
+  (frame :title "honinbo"
+         :on-close :exit
+         :content (border-panel :center (build-board)
+                                :south (build-controls))))
+
 (defn -main
   "This is the main entry point for launching honinbo **place holder**"
   [ & args ]
-  (-> (frame :title "hoinibo"
-             :on-close :exit
-             :content "Hello World!")
+  (-> (build-ui)
       pack!
       show!))
