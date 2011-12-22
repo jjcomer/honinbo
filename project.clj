b67362de69a4a2bd3ee4fb26dab14b7fa10234eb
Josh Comer <joshjcomer@gmail.com>
2011-12-22 07:45:56 -0400

    Updated the project dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject honinbo "0.0.1-SNAPSHOT"
   :description "A Go client client in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [seesaw "1.2.1"]]
+                 [seesaw "1.2.2"]]
   :dev-dependencies [[lein-marginalia "0.7.0-SNAPSHOT"]])
