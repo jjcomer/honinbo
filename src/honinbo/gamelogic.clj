98f9942b4aca0ba48ebac2d28fd7e53a57ebbef5
Josh Comer <joshjcomer@gmail.com>
2011-12-29 17:42:30 -0400

    Added some game logic... need to write some unit tests

---
+++ src/honinbo/gamelogic.clj
@@ -0,0 +1,42 @@
+(ns honinbo.gamelogic)
+
+(defn- neighbours
+  "This private function will return the valid neighbour of a given board
+   co-ordinate"
+  [size yx]
+  (let [deltas [[-1 0] [1 0] [0 -1] [0 1]]]
+    (filter (fn [new-yx]
+              (every? #(< -1 % size) new-yx))
+            (map #(map + yx %) deltas))))
+
+(defn get-opp-colour
+  [colour]
+  (case colour
+    :white :black
+    :black :white
+    :empty :empty))
+
+(defn gather-group
+  [yx size board]
+  (let [colour (get-in board yx)
+        opp-colour (get-opp-colour colour)]
+    (loop [group #{} to-visit [yx] board board]
+      (let [visit (peek to-visit)
+            visit-colour (get-in board (peek to-visit))]
+        (cond
+         (nil? visit) group
+         (= :visited visit-colour) (recur group (pop to-visit) board)
+         (= opp-colour visit-colour) (recur group (pop to-visit) board)
+         :else
+         (recur (conj group visit)
+                (apply conj (pop to-visit) (neighbours size visit))
+                (assoc-in board visit :visited)))))))
+
+(defn determine-capture
+  "Given the latest move location, this function will determine which
+   locations have been captured"
+  [yx size board]
+  (let [colour (get-opp-colour (get-in board yx))
+        n (filter #(= colour (get-in board %)) (neighbours size yx))]
+    (filter (fn [locs] (every? #(= colour (get-in board %)) locs))
+            (map #(gather-group % size board) n))))
